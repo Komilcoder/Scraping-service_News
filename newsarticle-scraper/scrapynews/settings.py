2dfd35264ceb5fe3ecd980597b6a7041e8d13085
yaxshilikovkomil <yaxshilikovkomil@gmail.com>
2023-09-28 19:08:47 +0500

    my scrapy file csv uploaded

--- newsarticle-scraper/scrapynews/settings.py
+++ newsarticle-scraper/scrapynews/settings.py
@@ -19,6 +19,9 @@ NEWSPIDER_MODULE = "scrapynews.spiders"
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+FEED_FORMAT = "csv"
+
+FEED_URI = "scrapynews.csv"
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
@@ -50,9 +53,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "scrapynews.middlewares.ScrapynewsDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "scrapynews.middlewares.ScrapynewsDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
