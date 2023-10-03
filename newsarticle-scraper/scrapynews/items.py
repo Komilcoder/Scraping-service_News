d1ab1fbd2eb625af268e3aced46d26e7c8d514a2
yaxshilikovkomil <yaxshilikovkomil@gmail.com>
2023-10-03 18:56:33 +0500

    last changes

--- newsarticle-scraper/scrapynews/items.py
+++ newsarticle-scraper/scrapynews/items.py
@@ -4,6 +4,8 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
 
 class ScrapynewsItem(scrapy.Item):
--- newsarticle-scraper/scrapynews/spiders/reuters.py
+++ newsarticle-scraper/scrapynews/spiders/reuters.py
@@ -1,24 +1,41 @@
 import scrapy
+from ..items import ScrapynewsItem
+from scrapy.loader import ItemLoader
 
 
 class ReutersSpider(scrapy.Spider):
     name = 'reuters'
     allowed_domains = ['kun.uz']
-    start_urls = ['https://kun.uz/en/news/category/politics']
+    start_urls = ['https://kun.uz/en/news/category/society']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print("parse", response)
-        print(response.xpath("//a[@class='menu-link']"), "sadasdsddddddd")
-        news = response.xpath("//div[@class='wrapper']")
-        print(news, "ooooooooooooooooo")
-        for new in news:
-            title = new.xpath("//span[@class='big-news__title']").get()
-            description = new.xpath("//span[@class='big-news__description']").get()
-            news_info = {
-                "title": title,
-                "description": description
-            }
-            yield news_info
-            # if news_info:
-            #     absl_url = f"https://kun.uz{news_info}"
-            #     yield scrapy.Request(url=absl_url, callback=self.parse)
+
+        news = response.css('div#news-list')
+        parent_div = response.xpath('//div[@id="news-list"]')
+
+        child_div = parent_div.xpath('//div[@class="news"]')
+        a_tags = child_div.xpath('.//a')
+
+        for a_tag in a_tags:
+            href = a_tag.xpath('@href').extract_first()
+            title = a_tag.xpath('text()').extract_first()
+
+        load_more_button = response.xpath('//div[@id="ias_trigger_1696323690503"]')
+        more_button = load_more_button.xpath('//div[@class="load-more__link"]')
+        if more_button:
+            yield scrapy.FormRequest.from_response(response, formdata={'button_id': 'ias_trigger_1696323690503'}, callback=self.parse)
+
+
+
+        # for new in news:
+        #     title = new.xpath("//span[@class='big-news__title']").get()
+        #     description = new.xpath("//span[@class='big-news__description']").get()
+        #     news_info = {
+        #         "title": title,
+        #         "description": description
+        #     }
+        #     yield news_info
